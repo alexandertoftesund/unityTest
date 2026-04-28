4b4bf99bdb248a6db7d3b01bdeab27d743a5a5c4
Alexander Toftesund <alexandertoftesund@gmail.com>
2026-04-28 18:05:18 +0200

    Level1_Manager cleanup

--- Assets/Scripts/SpikeDamage.cs
+++ Assets/Scripts/SpikeDamage.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Denne trengs for å starte på nytt
 
 public class SpikeDamage : MonoBehaviour
 {
-    [Header("Innstillinger")]
-    [Tooltip("Hvor mye liv spilleren mister ved berøring")]
-    public int damageAmount = 1;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) {
-            Debug.Log("AU! Spilleren traff piggene og mistet " + damageAmount + " liv.");
+            Debug.Log("Spilleren døde!");
+
+            // ALTERNATIV 1: Slett spilleren fra spillet (Instadød)
+            Destroy(other.gameObject);
+
+            // ALTERNATIV 2: Start hele brettet på nytt (nyttig for testing)
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
